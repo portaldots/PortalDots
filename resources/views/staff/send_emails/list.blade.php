b948eed819ec7ad86774b3dff8836c4cd2eb8867
SofPyon <sofpyon@hrgrweb.com>
2019-12-16 17:37:09 +0900

    お知らせがない時の表示を改善

--- resources/views/staff/send_emails/list.blade.php
+++ resources/views/staff/send_emails/list.blade.php
@@ -16,7 +16,7 @@
         </div>
         <div class="card-body">
 
-            <form method="post" action="{{ route('staff.send_emails') }}">
+            <form method="post" action="{{ route('staff.send_emails') }}" class="mb-1">
                 @csrf
                 @method('delete')
                 <button type="submit" class="btn btn-danger">
@@ -27,6 +27,11 @@
             <p class="text-muted">間違えてメールを配信してしまった場合、メールの配信を全てキャンセル(停止)できます。</p>
 
             <hr>
+
+            @if (count($pages) === 0)
+                <p class="text-muted lead text-center mb-1">お知らせがありません</p>
+                <p class="text-muted text-center">メールを一斉送信するには、まずお知らせを作成してください</p>
+            @endif
 
             @foreach ($pages as $page)
                 <details>
@@ -51,6 +56,12 @@
                 </details>
                 <hr>
             @endforeach
+
+            <p class="text-center">
+                <a href="{{ url('home_staff/pages') }}" class="btn btn-primary">
+                    お知らせ管理
+                </a>
+            </p>
         </div>
     </div>
 </div>
