22d8d7851701fcb1044261db594145c85677bdd8
SofPyon <sofpyon@hrgrweb.com>
2019-12-26 22:50:01 +0900

    いらないファイルを削除 #150

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 @endsection
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- resources/views/layouts/vue_app.blade.php
+++ resources/views/layouts/vue_app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
