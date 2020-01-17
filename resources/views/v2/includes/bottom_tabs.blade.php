b07f7c970a9cfde441dad62ec521a52b8edd3e97
SofPyon <sofpyon@hrgrweb.com>
2020-01-18 01:04:31 +0900

    下タブの「お問い合わせ」を「予定」に変更。ついでにproject v2の紹介も修正

--- resources/views/v2/includes/bottom_tabs.blade.php
+++ resources/views/v2/includes/bottom_tabs.blade.php
@@ -26,10 +26,10 @@
             <i class="far fa-edit bottom_tabs-tab__icon"></i>
             <div class="bottom_tabs-tab__label">申請</div>
         </a>
-        <a href="{{ route('contacts') }}" class="bottom_tabs-tab{{ Request::is('contacts*') ? ' is-active' : '' }}">
-            <i class="far fa-envelope bottom_tabs-tab__icon"></i>
-            <div class="bottom_tabs-tab__label">お問い合わせ</div>
-        </a>
         @endauth
+        <a href="{{ route('schedules.index') }}" class="bottom_tabs-tab{{ Request::is('schedules*') ? ' is-active' : '' }}">
+            <i class="far fa-calendar-alt bottom_tabs-tab__icon"></i>
+            <div class="bottom_tabs-tab__label">予定</div>
+        </a>
     </div>
 </div>
