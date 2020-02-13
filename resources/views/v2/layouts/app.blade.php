1ea1d8c1cec7ae4d3b4ab31e3b7bdd1259107fcf
SofPyon <sofpyon@hrgrweb.com>
2020-02-13 10:54:30 +0900

    JavaScript 無効時のメッセージを表示 #169

--- resources/views/v2/layouts/app.blade.php
+++ resources/views/v2/layouts/app.blade.php
@@ -45,7 +45,15 @@
 <body>
 
 <div class="loading" id="loading">
+    <div class="loading-noscript" id="js-noscript">JavaScript を有効にしてください</div>
     <div class="loading-circle"></div>
+    <script>
+        'use strict';
+        {
+            const noscript = document.getElementById('js-noscript');
+            noscript.parentNode.removeChild(noscript);
+        }
+    </script>
 </div>
 
 <div class="app" id="v2-app">
