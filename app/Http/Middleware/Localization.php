1e50c9acddc51185b44f1014981d28dbe3866005
SofPyon <sofpyon@hrgrweb.com>
2020-02-17 00:54:48 +0900

    ブラウザの言語設定を採用＆ログイン画面に言語切り替え #232

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -20,9 +20,15 @@ class Localization
     {
         $locale = 'ja';
         if (!empty($request->locale)) {
+            // ?locale=en などがついていれば、それを採用
             $locale = $request->locale;
         } elseif (!empty($request->cookie(self::LOCALE_COOKIE_NAME))) {
+            // 次に Cookie から取得
             $locale = $request->cookie(self::LOCALE_COOKIE_NAME);
+        } else {
+            // ブラウザの言語設定を採用
+            $accept_language = $request->server('HTTP_ACCEPT_LANGUAGE');
+            $locale = substr(explode(',', $accept_language)[0], 0, 2);
         }
         App::setLocale($locale);
         $response = $next($request);
