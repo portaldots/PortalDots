77690b299fa6c9a14511c5af50bb62896d476fe8
SofPyon <sofpyon@hrgrweb.com>
2020-03-28 20:39:35 +0900

    blade-formatterが余計なスペースを入れてしまう問題に対処

--- resources/views/staff/forms/editor.blade.php
+++ resources/views/staff/forms/editor.blade.php
@@ -5,7 +5,8 @@
 @section('title', 'フォームエディター - ' . config('app.name'))
     
 @section('content')
-    <div class="d-none" id="forms-editor-config" data-api-base-url='@json(route(' staff.forms.editor.api', [$form->id]))'>
+    <div class="d-none" id="forms-editor-config"
+        data-api-base-url='{{ json_encode(route('staff.forms.editor.api', [$form->id])) }}'>
     </div>
     <div id="forms-editor-container"></div>
 @endsection
