2e0eab9c9f54a0b4eda8285ecb614070b0ae1422
SofPyon <sofpyon@hrgrweb.com>
2020-03-29 01:02:32 +0900

    企画情報修正画面で例外発生するのを解消

--- app/Http/Controllers/Circles/EditAction.php
+++ app/Http/Controllers/Circles/EditAction.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Circles;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Eloquents\Circle;
+use App\Eloquents\CustomForm;
 
 class EditAction extends Controller
 {
@@ -12,7 +13,10 @@ class EditAction extends Controller
     {
         $this->authorize('circle.update', $circle);
 
+        $form = CustomForm::getFormByType('circle');
         return view('v2.circles.form')
-            ->with('circle', $circle);
+            ->with('circle', $circle)
+            ->with('form', $form)
+            ->with('questions', $form->questions()->get());
     }
 }
