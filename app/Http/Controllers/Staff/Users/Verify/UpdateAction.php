f7ab464334da78d317f7e098cea7f20a72ed8922
hosakou <hosa_kou@hotmail.co.jp>
2020-04-25 16:40:19 +0900

    削除し忘れていたものを削除

--- app/Http/Controllers/Staff/Users/Verify/UpdateAction.php
+++ app/Http/Controllers/Staff/Users/Verify/UpdateAction.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Staff\Users\Verify;
 use App\Eloquents\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class UpdateAction extends Controller
 {
