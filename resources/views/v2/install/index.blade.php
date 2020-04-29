fad20eecd68b1a207bf74cf5ba480739a6ba1a1a
SofPyon <sofpyon@hrgrweb.com>
2020-04-29 23:13:27 +0900

    インストールトップのデザイン調整＋4ステップ目追加

--- resources/views/v2/install/index.blade.php
+++ resources/views/v2/install/index.blade.php
@@ -7,32 +7,40 @@
 
     <app-container medium>
         <list-view>
-            <list-view-card>
-                <list-view-empty icon-class="fas fa-door-open" text="PortalDots へようこそ！">
-                    <p>
-                        はじめまして！<br>
-                        これから 3 ステップに分けて、PortalDots の設定をしましょう。
-                    </p>
-                    <steps-list>
-                        <steps-list-item>
-                            ポータルの名前<br>
-                            の設定
-                        </steps-list-item>
-                        <steps-list-item>
-                            データベース<br>
-                            の設定
-                        </steps-list-item>
-                        <steps-list-item>
-                            メール配信<br>
-                            の設定
-                        </steps-list-item>
-                    </steps-list>
-                    <div class="pt-spacing-lg">
-                        <a href="{{ route('install.portal.edit') }}" class="btn is-primary is-wide">
-                            <strong>インストールをはじめる</strong>
-                        </a>
-                    </div>
-                </list-view-empty>
+            <list-view-card class="text-center">
+                <p class="text-muted">
+                    <i class="fas fa-door-open fa-2x"></i>
+                </p>
+                <p>
+                    <strong>PortalDots へようこそ！</strong>
+                </p>
+                <p>
+                    はじめまして！<br>
+                    これから 4 ステップに分けて、PortalDots の設定をしましょう。
+                </p>
+                <steps-list>
+                    <steps-list-item>
+                        ポータルの名前<br>
+                        の設定
+                    </steps-list-item>
+                    <steps-list-item>
+                        データベース<br>
+                        の設定
+                    </steps-list-item>
+                    <steps-list-item>
+                        メール配信<br>
+                        の設定
+                    </steps-list-item>
+                    <steps-list-item>
+                        管理者ユーザー<br>
+                        の設定
+                    </steps-list-item>
+                </steps-list>
+                <div class="pt-spacing">
+                    <a href="{{ route('install.portal.edit') }}" class="btn is-primary is-wide">
+                        <strong>設定をはじめる</strong>
+                    </a>
+                </div>
             </list-view-card>
         </list-view>
     </app-container>
