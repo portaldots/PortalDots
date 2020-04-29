a112ea39eb95d0f330746f4dfc392d2d77e6a438
SofPyon <sofpyon@hrgrweb.com>
2020-04-29 23:13:47 +0900

    管理者ユーザー設定の4ステップ目を追加

--- resources/views/v2/includes/install_header.blade.php
+++ resources/views/v2/includes/install_header.blade.php
@@ -6,6 +6,8 @@ $step = 1;
 $step = 2;
 } elseif (Route::currentRouteName() === 'install.mail.edit' || Route::currentRouteName() === 'install.mail.test') {
 $step = 3;
+} elseif (Route::currentRouteName() === 'install.admin.create') {
+$step = 4;
 }
 @endphp
 
@@ -13,7 +15,7 @@ $step = 3;
     <template v-slot:title>
         PortalDots のインストール
         @if ($step > 0)
-            <small class="text-muted">(ステップ {{ $step }} / 3)</small>
+            <small class="text-muted">(ステップ {{ $step }} / 4)</small>
         @endif
     </template>
     @if ($step > 0)
@@ -21,6 +23,7 @@ $step = 3;
             <steps-list-item {{ $step === 1 ? 'active' : '' }}>ポータル情報</steps-list-item>
             <steps-list-item {{ $step === 2 ? 'active' : '' }}>データベース</steps-list-item>
             <steps-list-item {{ $step === 3 ? 'active' : '' }}>メール</steps-list-item>
+            <steps-list-item {{ $step === 4 ? 'active' : '' }}>管理者</steps-list-item>
         </steps-list>
     @endif
 </app-header>
