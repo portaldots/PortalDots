4f60329a3487449c9fdea68b97deafbc5cfb4c50
SofPyon <sofpyon@hrgrweb.com>
2020-05-04 01:07:27 +0900

    管理者アカウントが作成されたタイミングでインストール実行

--- app/Http/Controllers/Install/Admin/StoreAction.php
+++ app/Http/Controllers/Install/Admin/StoreAction.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers\Install\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\Install\RunInstallService;
 
 class StoreAction extends Controller
 {
+    /**
+     * @var RunInstallService
+     */
+    private $runInstallService;
+
+    public function __construct(RunInstallService $runInstallService)
+    {
+        $this->runInstallService = $runInstallService;
+    }
+
     public function __invoke()
     {
+        $this->runInstallService->run();
     }
 }
