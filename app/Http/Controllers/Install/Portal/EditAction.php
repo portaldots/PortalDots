9c216bd6762aa5fb4924f35b1217fe65c0967d7f
SofPyon <sofpyon@hrgrweb.com>
2020-05-04 18:03:52 +0900

    インストーラーのAPP_URL入力欄でhttp:http://と表示されてしまうことがあるのを修正

--- app/Http/Controllers/Install/Portal/EditAction.php
+++ app/Http/Controllers/Install/Portal/EditAction.php
@@ -20,8 +20,14 @@ class EditAction extends Controller
 
     public function __invoke(Request $request)
     {
+        $url = url('/');
+
+        if (strpos($url, 'http:') !== 0 && strpos($url, 'https:') !== 0) {
+            $url = ($request->isSecure() ? 'https:' : 'http:') . $url;
+        }
+
         return view('v2.install.portal.form')
             ->with('portal', $this->portalService->getInfo())
-            ->with('suggested_app_url', ($request->isSecure() ? 'https:' : 'http:') . url('/'));
+            ->with('suggested_app_url', $url);
     }
 }
