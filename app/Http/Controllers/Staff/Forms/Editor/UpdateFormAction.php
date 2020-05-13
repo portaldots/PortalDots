d0b38fe81fdad3d611e7363face4801e3e07e293
SofPyon <sofpyon@hrgrweb.com>
2020-05-14 00:07:33 +0900

    カスタムフォームのフォームタイトル・説明を修正しようとした場合エラーが発生するように

--- app/Http/Controllers/Staff/Forms/Editor/UpdateFormAction.php
+++ app/Http/Controllers/Staff/Forms/Editor/UpdateFormAction.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Staff\Forms\Editor;
 use App\Services\Forms\FormsService;
 use App\Http\Requests\Staff\Forms\Editor\UpdateFormRequest;
 use App\Http\Controllers\Controller;
+use App\Eloquents\Form;
 
 class UpdateFormAction extends Controller
 {
@@ -21,6 +22,12 @@ class UpdateFormAction extends Controller
     public function __invoke(int $form_id, UpdateFormRequest $request)
     {
         $form = $request->form;
+
+        // カスタムフォームのフォーム情報は修正禁止
+        if (isset(Form::findOrFail($form['id'])->customForm)) {
+            return abort(400);
+        }
+
         unset($form['created_by'], $form['created_at'], $form['updated_at'], $form['custom_form'], $form['id']);
 
         $this->formsService->updateForm(
