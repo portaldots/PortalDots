38892a2e4809ba10d59e9c98f52c7e99e6ada7bd
hosakou <hosa_kou@hotmail.co.jp>
2020-05-19 08:53:20 +0900

    複製処理を分離

--- app/Http/Controllers/Staff/Forms/CopyAction.php
+++ app/Http/Controllers/Staff/Forms/CopyAction.php
@@ -5,32 +5,28 @@ namespace App\Http\Controllers\Staff\Forms;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Eloquents\Form;
+use App\Services\Forms\FormsService;
 use Illuminate\Support\Facades\Auth;
 
 class CopyAction extends Controller
 {
+    public $formsService;
+
+    public function __construct(FormsService $formsService)
+    {
+        $this->formsService = $formsService;
+    }
+
     public function __invoke(Form $form)
     {
-        $form_copy = $form->replicate()->fill([
-            'name' => $form->name . 'のコピー',
-            'created_by' => Auth::id(),
-            'is_public' => false,
-        ]);
+        $form_copy = $this->formsService->copyForm($form);
 
-        if (! $form_copy->save()) {
+        if (empty($form_copy)) {
             return redirect()
-                ->route('staff.forms.copy', ['form' => $form])
-                ->with('topAlert.title', '保存に失敗しました')
-                ->with('topAlert.type', 'danger');
+            ->route('staff.forms.copy', ['form' => $form])
+            ->with('topAlert.title', '保存に失敗しました')
+            ->with('topAlert.type', 'danger');
         }
-
-        $questions = $form->questions()->get();
-
-        $questions_copy = $questions->map(function ($question) use ($form_copy) {
-            return $question->replicate(['form_id']);
-        });
-
-        $form_copy->questions()->createMany($questions_copy->toArray());
 
         return redirect("/home_staff/applications/read/{$form_copy->id}?copied=1");
     }
--- app/Services/Forms/FormsService.php
+++ app/Services/Forms/FormsService.php
@@ -6,6 +6,7 @@ namespace App\Services\Forms;
 
 use App\Eloquents\Form;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class FormsService
 {
@@ -22,5 +23,33 @@ class FormsService
         $form['close_at'] = new Carbon($form['close_at']);
         $eloquent->fill($form);
         $eloquent->save();
+    }
+
+    /**
+     * フォームを複製する
+     *
+     * @param Form $form
+     * @return Form|null
+     */
+    public function copyForm(Form $form)
+    {
+        $form_copy = $form->replicate()->fill([
+            'name' => $form->name . 'のコピー',
+            'created_by' => Auth::id(),
+            'is_public' => false,
+        ]);
+
+        if ($form_copy->save()) {
+            $questions = $form->questions()->get();
+            $questions_copy = $questions->map(function ($question) use ($form_copy) {
+                return $question->replicate(['form_id']);
+            });
+
+            $form_copy->questions()->createMany($questions_copy->toArray());
+
+            return $form_copy;
+        }
+
+        return null;
     }
 }
