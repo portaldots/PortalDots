9b571798c7da6e79e5ad2eb8b2f14da764bd139f
SofPyon <sofpyon@hrgrweb.com>
2020-05-23 01:28:42 +0900

    カスタムフォーム設定関連のテストクラスを新規作成

---
+++ tests/Feature/Http/Controllers/Staff/Circles/CustomForm/IndexActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Staff\Circles\CustomForm;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class IndexActionTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
