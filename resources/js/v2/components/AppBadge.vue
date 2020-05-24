2d7ff98ded22f509547ade1c48bb2d7770111f5e
hosakou <hosa_kou@hotmail.co.jp>
2020-05-24 13:11:35 +0900

    Badge のコンポーネントを作成

---
+++ resources/js/v2/components/AppBadge.vue
@@ -0,0 +1,63 @@
+<template>
+  <span
+    class="badge"
+    :class="{
+      'is-primary': primary,
+      'is-danger': danger,
+      'is-success': success,
+      'is-muted': muted
+    }"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    primary: {
+      type: Boolean,
+      default: false
+    },
+    danger: {
+      type: Boolean,
+      default: false
+    },
+    success: {
+      type: Boolean,
+      default: false
+    },
+    muted: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.badge {
+  border-radius: $border-radius;
+  display: inline-block;
+  font-size: 0.75em;
+  line-height: 1.75;
+  padding: 0 0.4em;
+  text-align: center;
+  &.is-primary {
+    background: $color-primary;
+    color: #fff;
+  }
+  &.is-danger {
+    background: $color-danger;
+    color: #fff;
+  }
+  &.is-success {
+    background: $color-success;
+    color: #fff;
+  }
+  &.is-muted {
+    background: $color-muted;
+    color: #fff;
+  }
+}
+</style>
