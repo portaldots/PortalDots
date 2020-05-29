b221260c8f66f3fd06fae88d4943abca55c9b1e8
hosakou <hosa_kou@hotmail.co.jp>
2020-05-29 21:37:13 +0900

    常に下に表示されるように

--- resources/views/v2/includes/footer.blade.php
+++ resources/views/v2/includes/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="footer">
     <hr />
-    <p class="footer__credit">
+    <div class="footer__credit">
         Powered by
         <a href="https://github.com/portal-dots/PortalDots" class="footer__link" target="_blank" rel="noopener">
             PortalDots
         </a>
-    </p>
+    </div>
 </div>
