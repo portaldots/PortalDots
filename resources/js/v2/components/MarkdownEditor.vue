a1e03e0572555205fe77fa6ca957ea1f7db38ade
SofPyon <sofpyon@hrgrweb.com>
2020-05-30 18:02:13 +0900

    Markdownエディタコンポーネントを新規作成

---
+++ resources/js/v2/components/MarkdownEditor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <vue-simplemde v-model="content" ref="markdownEditor" />
+    <input
+      type="hidden"
+      :name="inputName"
+      :value="this.content"
+      v-if="inputName"
+    />
+  </div>
+</template>
+
+<script>
+import VueSimplemde from 'vue-simplemde'
+
+export default {
+  components: {
+    VueSimplemde
+  },
+  data() {
+    return {
+      content: ''
+    }
+  },
+  props: {
+    inputName: {
+      type: String,
+      default: null
+    },
+    defaultValue: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted() {
+    this.content = this.defaultValue
+  }
+}
+</script>
+
+<style>
+@import '~simplemde/dist/simplemde.min.css';
+</style>
