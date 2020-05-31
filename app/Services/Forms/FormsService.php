aeb0b04ebb0b3a7396b546f377eec2216caed4c4
hosakou <hosa_kou@hotmail.co.jp>
2020-05-31 17:18:14 +0900

    transaction を利用した書き方に

--- app/Services/Forms/FormsService.php
+++ app/Services/Forms/FormsService.php
@@ -7,6 +7,7 @@ namespace App\Services\Forms;
 use App\Eloquents\Form;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FormsService
 {
@@ -33,22 +34,23 @@ class FormsService
      */
     public function copyForm(Form $form)
     {
-        $form_copy = $form->replicate()->fill([
-            'name' => $form->name . 'のコピー',
-            'created_by' => Auth::id(),
-            'is_public' => false,
-        ]);
+        return DB::transaction(function () use ($form) {
+            $form_copy = $form->replicate()->fill([
+                'name' => $form->name . 'のコピー',
+                'created_by' => Auth::id(),
+                'is_public' => false,
+            ]);
 
-        if ($form_copy->save()) {
+            $form_copy->save();
+
             $questions = $form->questions()->get();
-            $questions_copy = $questions->map(function ($question) use ($form_copy) {
+            $questions_copy = $questions->map(function ($question) {
                 return $question->replicate(['form_id']);
             });
 
             $form_copy->questions()->createMany($questions_copy->toArray());
-
             return $form_copy;
-        }
+        });
 
         return null;
     }
