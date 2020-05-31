a206c9af9a987e97ef58e26f81264ce2b1464242
hosakou <hosa_kou@hotmail.co.jp>
2020-05-31 23:19:22 +0900

    コントローラーのテストを作成

---
+++ tests/Feature/Http/Controllers/Staff/Forms/CopyActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Staff\Forms;
+
+use App\Eloquents\Form;
+use App\Eloquents\User;
+use App\Services\Forms\FormsService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Mockery;
+
+class CopyActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->form = factory(Form::class)->create();
+        $this->staff = factory(User::class)->states('staff')->create();
+    }
+
+    /**
+     * @test
+     */
+    public function FormsServiceのcopyFormが呼び出せる()
+    {
+        $this->mock(FormsService::class, function ($mock) {
+            $mock->shouldReceive('copyForm')->once()->andReturn($this->form);
+        });
+
+        $responce = $this->actingAs($this->staff)
+            ->withSession(['staff_authorized' => true])
+            ->post(route('staff.forms.copy', ['form' => $this->form]));
+
+        // CodeIgniter を廃止したら↓を書き換える
+        $responce->assertRedirect("/home_staff/applications/read/1?copied=1");
+    }
+}
