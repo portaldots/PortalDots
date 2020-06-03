0127e4060861831a056f59f7f3e4ca135c29748d
Soji Takahashi <sofpyon@hrgrweb.com>
2020-06-03 15:59:22 +0900

    【DownloadZipServiceTest】use RefreshDatabaseのコメントアウトを解除 #126 (#134)
    * 【DownloadZipServiceTest】use RefreshDatabaseのコメントアウトを解除 #126
    
    * use RefreshDatabase を追加
    
    * コードの変更に強くするため、copyForm関数実行時にはコピー実行者ユーザーも引数として渡す

--- tests/Feature/Http/Controllers/Staff/Forms/CopyActionTest.php
+++ tests/Feature/Http/Controllers/Staff/Forms/CopyActionTest.php
@@ -29,7 +29,9 @@ class CopyActionTest extends TestCase
     {
         $this->mock(FormsService::class, function ($mock) {
             $mock->shouldReceive('copyForm')->once()->with(Mockery::on(function ($arg) {
-                return $this->form->id === $arg->id;
+                return $this->form->id === $arg->id && $this->form->name === $arg->name;
+            }), Mockery::on(function ($arg) {
+                return $this->staff->id === $arg->id && $this->staff->name === $arg->name;
             }))->andReturn($this->form_copy);
         });
 
