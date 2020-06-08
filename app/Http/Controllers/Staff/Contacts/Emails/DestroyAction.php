49e028202d2b2f823abd41c333ba1e5653e64f5a
hosakou <hosa_kou@hotmail.co.jp>
2020-06-09 00:11:01 +0900

    コントローラーつくった

---
+++ app/Http/Controllers/Staff/Contacts/Emails/DestroyAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Staff\Contacts\Emails;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DestroyAction extends Controller
+{
+    //
+}
