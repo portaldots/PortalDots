6a7288c06c8dd1364ae9cf367b87c58d943d7a58
hosakou <hosa_kou@hotmail.co.jp>
2020-06-09 18:55:51 +0900

    責任者情報も出力

--- app/Exports/CirclesExport.php
+++ app/Exports/CirclesExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Eloquents\Circle;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class CirclesExport implements FromCollection, WithHeadings
+class CirclesExport implements FromCollection, WithHeadings, WithMapping
 {
     private $headings = [
         'id' => 'id',
@@ -16,6 +17,9 @@ class CirclesExport implements FromCollection, WithHeadings
         'group_name_yomi' => '団体名（よみ）',
         'status' => 'ステータス',
         'notes' => 'スタッフメモ',
+        'leader_id' => '責任者 ユーザーID',
+        'leader_student_id' => '責任者 学籍番号',
+        'leader_name' => '責任者 氏名',
     ];
 
     /**
@@ -23,7 +27,28 @@ class CirclesExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        return Circle::Submitted()->get(array_keys($this->headings));
+        return Circle::Submitted()->with('leader')->get();
+    }
+
+    /**
+    * @var Circle $circle
+    */
+    public function map($circle): array
+    {
+        $leader = $circle->leader->first();
+
+        return [
+            'id' => $circle->id,
+            'name' => $circle->name,
+            'name_yomi' => $circle->name_yomi,
+            'group_name' => $circle->group_name,
+            'group_name_yomi' => $circle->group_name_yomi,
+            'status' => $circle->status,
+            'notes' => $circle->notes,
+            'leader_id' => $leader->id ?? null,
+            'leader_student_id' => $leader->student_id ?? null,
+            'leader_name' => $leader->name ?? null,
+        ];
     }
 
     /**
