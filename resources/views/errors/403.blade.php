b5d5550099684e18f8f874118d6ae96b520d146f
SofPyon <sofpyon@hrgrweb.com>
2020-06-14 02:22:37 +0900

    複数企画に所属しているユーザーの場合、ある企画でログインしているときは403でも他の企画では閲覧可能な場合があるので、その旨表示

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,6 +1,18 @@
+@inject('selectorService', 'App\Services\Circles\SelectorService')
+
 @extends('errors.layout')
 
 @section('title', '403 Forbidden')
 @section('top', $exception->getMessage() ?: 'アクセスが拒否されました')
-@section('message', '権限がないか、アクセスできないページです')
+@section('message')
+    <p>権限がないか、アクセスできないページです</p>
+    @auth
+        @if (count($selectorService->getSelectableCirclesList(Auth::user(), Request::path())) >= 2)
+            <p>
+                <i class="fas fa-info-circle fa-fw"></i>
+                ログイン先の企画を変更するとアクセスできる場合があります
+            </p>
+        @endif
+    @endauth
+@endsection
 @section('twitter', false)
