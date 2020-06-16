8027c57daf1525fb3f6743c9b43bcf2723e02c47
hosakou <hosa_kou@hotmail.co.jp>
2020-06-17 00:20:02 +0900

    提出後に「変更」と出ないように

--- resources/views/v2/includes/circle_info.blade.php
+++ resources/views/v2/includes/circle_info.blade.php
@@ -6,14 +6,14 @@
         'group_name_yomi' => '企画を出店する団体の名称(よみ)',
         ] as $field_name => $display_name)
         <dt>{{ $display_name }}
-            @if (Auth::user()->isLeaderInCircle($circle))
+            @if (Auth::user()->isLeaderInCircle($circle) && Gate::allows('circle.update', $circle))
             — <a href="{{ route('circles.edit', ['circle' => $circle]) }}">変更</a>
             @endif
         </dt>
         <dd>{{ $circle->$field_name }}</dd>
     @endforeach
     <dt>メンバー
-        @if (Auth::user()->isLeaderInCircle($circle))
+        @if (Auth::user()->isLeaderInCircle($circle) && Gate::allows('circle.update', $circle))
          — <a href="{{ route('circles.users.index', ['circle' => $circle]) }}">変更</a>
         @endif
     </dt>
