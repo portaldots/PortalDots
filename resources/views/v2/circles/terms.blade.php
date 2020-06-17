38584c42bcf2d8feee6a4811d37b572047c5c5b9
hosakou <hosa_kou@hotmail.co.jp>
2020-06-18 04:32:36 +0900

    修正 AppHeaderのタイトルを変更 LieViewにタイトルを設定 ボタンの文字を太字に

--- resources/views/v2/circles/terms.blade.php
+++ resources/views/v2/circles/terms.blade.php
@@ -6,17 +6,18 @@
 
 @section('content')
 <app-header container-medium>
-    <template v-slot:title>企画参加登録を始める前に</template>
+    <template v-slot:title>企画参加登録</template>
 </app-header>
 <app-container medium>
     <list-view>
+        <template v-slot:title>企画参加登録を始める前に</template>
         <list-view-card class="markdown">
             @markdown($description)
         </list-view-card>
     </list-view>
     <div class="text-center pt-spacing-md">
         <a class="btn is-primary is-wide" href="{{ route('circles.create') }}">
-            企画参加登録を始める
+            <strong>参加登録をはじめる</strong>
         </a>
     </div>
 </app-container>
