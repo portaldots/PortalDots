787bd8e065696999af4434c206706910d4add966
hosakou <hosa_kou@hotmail.co.jp>
2020-06-18 22:57:10 +0900

    Descriptionが設定されている時はCreateActionに直接アクセスできないように

--- app/Http/Controllers/Circles/CreateAction.php
+++ app/Http/Controllers/Circles/CreateAction.php
@@ -13,6 +13,17 @@ class CreateAction extends Controller
         $this->authorize('circle.create');
 
         $form = CustomForm::getFormByType('circle');
+
+        if (
+            isset($form->description)
+            && url()->previous() !== config('app.url') . route('circles.terms', [], false)
+            && url()->previous() !== config('app.url') . route('circles.create', [], false)
+            && url()->previous() !== config('app.url') . route('circles.store', [], false)
+        ) {
+            return redirect()
+                ->route('circles.terms');
+        }
+
         return view('v2.circles.form')
             ->with('form', $form)
             ->with('questions', $form->questions()->get());
