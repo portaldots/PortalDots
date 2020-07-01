526cf004da495ed419d7e7ed7c9f8733f8ca5647
hosakou <hosa_kou@hotmail.co.jp>
2020-07-01 21:08:38 +0900

    途中

--- resources/views/includes/loading.blade.php
+++ resources/views/includes/loading.blade.php
@@ -1,11 +1,17 @@
 <div class="loading" id="loading">
     <div class="loading-noscript" id="js-noscript">JavaScript を有効にしてください</div>
+    <div class="loading-isie" id="is-ie">
+        お使いのブラウザには対応していません
+    </div>
     <div class="loading-circle"></div>
     <script>
         'use strict'; {
             const noscript = document.getElementById('js-noscript');
             noscript.parentNode.removeChild(noscript);
         }
-
+        if (navigator.userAgent.match(/msie|trident|edge/i)) {
+            document.getElementById("is-ie").style.display = "block";
+            document.getElementById('loading').parentNode.removeChild(document.getElementsByClassName('loading-circle'));
+        }
     </script>
 </div>
