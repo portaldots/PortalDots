eaa5584f5839cbb56fe156441731abbc8cdb0819
hosakou <hosa_kou@hotmail.co.jp>
2020-07-12 18:35:30 +0900

    修正 アイコンを入れた 以降の処理を中断するようにした

--- resources/views/includes/loading.blade.php
+++ resources/views/includes/loading.blade.php
@@ -1,7 +1,9 @@
 <div class="loading" id="loading">
     <div class="loading-noscript" id="js-noscript">JavaScript を有効にしてください</div>
     <div class="loading-isie" id="is-ie">
-        お使いのブラウザには対応していません
+        <i class="fas fa-exclamation-circle"></i> お使いのブラウザーはサポートされていません
+        <br/>
+        最新の Microsoft Edge や Google Chrome からアクセスしてください
     </div>
     <div class="loading-circle" id="loading-circle"></div>
     <script>
@@ -13,6 +15,7 @@
             const isie = document.getElementById("is-ie");
             isie.style.display = "block";
             isie.parentNode.removeChild(document.getElementById('loading-circle'));
+            window.stop();
         }
     </script>
 </div>
