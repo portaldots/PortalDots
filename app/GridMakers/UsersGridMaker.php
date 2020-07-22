59f0c969b30eb7141e2de360c1ff16d6b3802490
hosakou <hosa_kou@hotmail.co.jp>
2020-07-22 15:20:03 +0900

    v2 にも表示するように

--- app/GridMakers/UsersGridMaker.php
+++ app/GridMakers/UsersGridMaker.php
@@ -7,6 +7,7 @@ namespace App\GridMakers;
 use Illuminate\Database\Eloquent\Builder;
 use App\Eloquents\User;
 use App\GridMakers\Concerns\UseEloquent;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class UsersGridMaker implements GridMakable
@@ -39,6 +40,7 @@ class UsersGridMaker implements GridMakable
             'is_admin',
             'email_verified_at',
             'univemail_verified_at',
+            'last_accessed_at',
             'notes',
             'created_at',
             'updated_at',
@@ -63,6 +65,7 @@ class UsersGridMaker implements GridMakable
             'is_admin' => ['type' => 'bool'],
             'email_verified_at' => ['type' => 'isNull'],
             'univemail_verified_at' => ['type' => 'isNull'],
+            'last_accessed_at' => ['type' => 'datetime'],
             'notes' => ['type' => 'string'],
             'created_at' => ['type' => 'datetime'],
             'updated_at' => ['type' => 'datetime'],
@@ -87,6 +90,7 @@ class UsersGridMaker implements GridMakable
             'is_admin',
             'email_verified_at',
             'univemail_verified_at',
+            'last_accessed_at',
             'notes',
             'created_at',
             'updated_at',
@@ -101,6 +105,9 @@ class UsersGridMaker implements GridMakable
         $item = [];
         foreach ($this->keys() as $key) {
             switch ($key) {
+                case 'last_accessed_at':
+                    $item[$key] = $this->formatLastAccessedAt($record->last_accessed_at);
+                    break;
                 case 'created_at':
                     $item[$key] = $record->created_at->format('Y/m/d H:i:s');
                     break;
@@ -117,5 +124,30 @@ class UsersGridMaker implements GridMakable
     protected function model(): Model
     {
         return new User();
+    }
+
+    /**
+     * 最終アクセスを変換して返す関数
+     * @param Carbon|null $last_accessed_at
+     * @return String
+     */
+    private function formatLastAccessedAt(?Carbon $last_accessed_at)
+    {
+        if (empty($last_accessed_at)) {
+            return "-";
+        }
+        if (now()->subHour()->lte($last_accessed_at)) {
+            return '1時間以内';
+        }
+        if (now()->subDay()->lte($last_accessed_at)) {
+            return "{$last_accessed_at->diffInHours(now())}時間前";
+        }
+        if (now()->subMonth()->lte($last_accessed_at)) {
+            return "{$last_accessed_at->diffInDays(now())}日前";
+        }
+        if (now()->subYear()->lte($last_accessed_at)) {
+            return "{$last_accessed_at->diffInMonths(now())}ヶ月前";
+        }
+        return "1年以上前";
     }
 }
