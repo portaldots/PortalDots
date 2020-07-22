e574602ed70db5677fdb51ddde756f0e175c9acd
hosakou <hosa_kou@hotmail.co.jp>
2020-07-22 15:36:37 +0900

    テストに 最終アクセス も追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'univemail_verified_at' => now(),
         'signed_up_at' => now(),
+        'last_accessed_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
--- tests/Feature/GridMakers/UsersGridMakerTest.php
+++ tests/Feature/GridMakers/UsersGridMakerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\GridMakers;
 
 use App\Eloquents\User;
 use App\GridMakers\UsersGridMaker;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\App;
 use Tests\TestCase;
 
@@ -19,6 +21,8 @@ class UsersGridMakerTest extends TestCase
         parent::setUp();
 
         $this->usersGridMaker = App::make(UsersGridMaker::class);
+        Carbon::setTestNow(new CarbonImmutable('2020-02-08 00:00:00'));
+        CarbonImmutable::setTestNow(new CarbonImmutable('2020-02-08 00:00:00'));
     }
 
     /**
@@ -27,12 +31,14 @@ class UsersGridMakerTest extends TestCase
     public function map()
     {
         $user = factory(User::class)->make([
+            'last_accessed_at' => '2020-02-02 02:02:02',
             'created_at' => '2020-02-02 02:02:02',
             'updated_at' => '2020-02-02 02:02:02',
         ]);
 
         $result = $this->usersGridMaker->map($user);
 
+        $this->assertSame('5日前', $result['last_accessed_at']); // 02:02:02 を迎えていないので 5日前 と返ってくる
         $this->assertSame('2020/02/02 02:02:02', $result['created_at']);
         $this->assertSame('2020/02/02 02:02:02', $result['updated_at']);
     }
