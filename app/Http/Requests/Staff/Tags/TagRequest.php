480fafd03527ef3cd26cbfe058f4eea0af8f7519
SofPyon <sofpyon@hrgrweb.com>
2020-08-26 22:18:48 +0900

    タグ作成・編集ページをv2化

---
+++ app/Http/Requests/Staff/Tags/TagRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Staff\Tags;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションエラーのカスタム属性の取得
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => 'タグ',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', Rule::unique('tags')->ignore($this->tag ?? null)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => '同名のタグが既に存在します。',
+        ];
+    }
+}
