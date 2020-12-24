27e33dc37f65178ea20691cde73d0335eee83f3a
SofPyon <sofpyon@hrgrweb.com>
2020-12-25 00:21:57 +0900

    Grocery CRUDの「操作」ボタンがカラフルすぎて目がチカチカするため、単色に

--- public/assets/grocery_crud/themes/crud-bootstrap/views/list.php
+++ public/assets/grocery_crud/themes/crud-bootstrap/views/list.php
@@ -1,85 +1,83 @@
 <?php
 
-	$column_width = (int)(80/count($columns));
+    $column_width = (int)(80/count($columns));
 
-	if(!empty($list)){
-?><div class="bDiv" >
+    if (!empty($list)) {
+        ?><div class="bDiv" >
 		<table class="table" cellspacing="0" cellpadding="0" border="0" id="flex1">
 		<thead>
 			<tr class='hDiv'>
-				<?php if(!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)){?>
+				<?php if (!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)) {?>
 				<th align="left" abbr="tools" axis="col1" class="" width='20%'>
 					<div class="text-left">
 						<?php echo $this->l('list_actions'); ?>
 					</div>
 				</th>
-				<?php }?>
-				<?php foreach($columns as $column){?>
+				<?php } ?>
+				<?php foreach ($columns as $column) {?>
 				<th width='<?php echo $column_width?>%'>
-					<div class="text-left field-sorting <?php if(isset($order_by[0]) &&  $column->field_name == $order_by[0]){?><?php echo $order_by[1]?><?php }?>"
+					<div class="text-left field-sorting <?php if (isset($order_by[0]) &&  $column->field_name == $order_by[0]) {?><?php echo $order_by[1]?><?php }?>"
 						rel='<?php echo $column->field_name?>'>
 						<?php echo $column->display_as?>
 					</div>
 				</th>
-				<?php }?>
+				<?php } ?>
 			</tr>
 		</thead>
 		<tbody>
-<?php foreach($list as $num_row => $row){ ?>
-		<tr  <?php if($num_row % 2 == 1){?>class="erow"<?php }?>>
-			<?php if(!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)){?>
+<?php foreach ($list as $num_row => $row) { ?>
+		<tr  <?php if ($num_row % 2 == 1) {?>class="erow"<?php }?>>
+			<?php if (!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)) {?>
 			<td align="left" width='20%'>
 				<div class='tools'>
-					<?php if(!$unset_delete && empty($delete_confirm_url)){?>
-						<a href='<?php echo $row->delete_url?>' title='<?php echo $this->l('list_delete')?> <?php echo $subject?>' class="btn btn-danger delete-row">
+					<?php if (!$unset_delete && empty($delete_confirm_url)) {?>
+						<a href='<?php echo $row->delete_url?>' title='<?php echo $this->l('list_delete')?> <?php echo $subject?>' class="btn text-muted delete-row">
 							<i class="fa fa-trash" aria-hidden="true"></i>
 						</a>
 					<?php }?>
-					<?php if(!empty($delete_confirm_url)){?>
-						<a href='<?php echo str_replace('{{id}}', $row->id, $delete_confirm_url) ?>' title='削除' class="btn btn-danger delete-tag-row">
+					<?php if (!empty($delete_confirm_url)) {?>
+						<a href='<?php echo str_replace('{{id}}', $row->id, $delete_confirm_url) ?>' title='削除' class="btn text-muted delete-tag-row">
 							<i class="fa fa-trash" aria-hidden="true"></i>
 						</a>
 					<?php }?>
-					<?php if(!$unset_edit){?>
-						<a href='<?php echo $row->edit_url?>' title='<?php echo $this->l('list_edit')?> <?php echo $subject?>' class="btn btn-primary edit-row">
+					<?php if (!$unset_edit) {?>
+						<a href='<?php echo $row->edit_url?>' title='<?php echo $this->l('list_edit')?> <?php echo $subject?>' class="btn text-muted edit-row">
 							<i class="fa fa-pencil" aria-hidden="true"></i>
 						</a>
 					<?php }?>
-					<?php if($set_editor){?>
-						<a href='<?php echo base_url($row->editor_url) ?>' title='エディターで編集' class="btn btn-primary edit-row">
+					<?php if ($set_editor) {?>
+						<a href='<?php echo base_url($row->editor_url) ?>' title='エディターで編集' class="btn text-muted edit-row">
 							<i class="fa fa-edit" aria-hidden="true"></i>
 						</a>
 					<?php }?>
-					<?php if(!$unset_read){?>
-						<a href='<?php echo $row->read_url?>' title='<?php echo $this->l('list_view')?> <?php echo $subject?>' class="btn btn-info view-row">
+					<?php if (!$unset_read) {?>
+						<a href='<?php echo $row->read_url?>' title='<?php echo $this->l('list_view')?> <?php echo $subject?>' class="btn text-muted view-row">
 							<i class="fa fa-eye" aria-hidden="true"></i>
 						</a>
 					<?php }?>
-					<?php if($set_copy_url){?>
-						<a href='<?php echo $row->copy_form_url ?>' title='複製' class="btn btn-warning">
+					<?php if ($set_copy_url) {?>
+						<a href='<?php echo $row->copy_form_url ?>' title='複製' class="btn text-muted">
 							<i class="fa fa-copy" aria-hidden="true"></i>
 						</a>
 					<?php }?>
 					<?php
-					if(!empty($row->action_urls)){
-						foreach($row->action_urls as $action_unique_id => $action_url){
-							$action = $actions[$action_unique_id];
-					?>
+                    if (!empty($row->action_urls)) {
+                        foreach ($row->action_urls as $action_unique_id => $action_url) {
+                            $action = $actions[$action_unique_id]; ?>
 							<a href="<?php echo $action_url; ?>" class="<?php echo $action->css_class; ?> crud-action" title="<?php echo $action->label?>"><?php
-								if(!empty($action->image_url))
-								{
-									?><img src="<?php echo $action->image_url; ?>" alt="<?php echo $action->label?>" /><?php
-								}
-							?></a>
-					<?php }
-					}
-					?>
+                                if (!empty($action->image_url)) {
+                                    ?><img src="<?php echo $action->image_url; ?>" alt="<?php echo $action->label?>" /><?php
+                                } ?></a>
+					<?php
+                        }
+                    }
+                    ?>
 										<div class='clear'></div>
 				</div>
 			</td>
 			<?php }?>
-			<?php foreach($columns as $column){?>
-			<td width='<?php echo $column_width?>%' class='<?php if(isset($order_by[0]) &&  $column->field_name == $order_by[0]){?>sorted<?php }?>'>
+			<?php foreach ($columns as $column) {?>
+			<td width='<?php echo $column_width?>%' class='<?php if (isset($order_by[0]) &&  $column->field_name == $order_by[0]) {?>sorted<?php }?>'>
 				<div class='text-left'><?php echo $row->{$column->field_name} != '' ? $row->{$column->field_name} : '&nbsp;' ; ?></div>
 			</td>
 			<?php }?>
@@ -88,7 +86,8 @@
 		</tbody>
 		</table>
 	</div>
-<?php }else{?>
+<?php
+    } else {?>
 	<br/>
 	&nbsp;&nbsp;&nbsp;&nbsp; <?php echo $this->l('list_no_items'); ?>
 	<br/>
