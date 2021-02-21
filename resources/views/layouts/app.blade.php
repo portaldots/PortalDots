22ca33de36c9cb8c80b578442e1aa59abbd8c512
SofPyon <sofpyon@hrgrweb.com>
2021-02-21 23:07:20 +0900

    ブラウザ標準のUIがダークモードでダーク表示されるように

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="color-scheme" content="light dark">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="turbolinks-cache-control" content="no-cache">
     <title>
