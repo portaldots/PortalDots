69bd8474a39301bf7417d3896dc8aedda0498b4b
SofPyon <sofpyon@hrgrweb.com>
2021-02-23 19:10:49 +0900

    バージョンは手動でセットする必要がないことを追記

--- app/ReleaseInfo.php
+++ app/ReleaseInfo.php
@@ -10,7 +10,7 @@ class ReleaseInfo
     /**
      * PortalDots のバージョン
      *
-     * PortalDots リリース ZIP 作成時、以下の定数にバージョンがセットされます。
+     * GitHub Actions により、PortalDots リリース ZIP 作成時、以下の定数にバージョンが自動でセットされます。
      * 例 : '1.0.0' (先頭に v をつけて 'v1.0.0' とはしません)
      */
     public const VERSION = '###VERSION_PLACEHOLDER###';
