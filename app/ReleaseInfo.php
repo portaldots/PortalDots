2ff16012bbd1fa364b013dd763a2e6361b0fa416
SofPyon <sofpyon@hrgrweb.com>
2021-02-23 18:45:07 +0900

    リリースZIP作成時にバージョン番号をファイルにセット

--- app/ReleaseInfo.php
+++ app/ReleaseInfo.php
@@ -11,6 +11,7 @@ class ReleaseInfo
      * PortalDots のバージョン
      *
      * PortalDots リリース ZIP 作成時、以下の定数にバージョンがセットされます。
+     * 例 : '1.0.0' (先頭に v をつけて 'v1.0.0' とはしません)
      */
     public const VERSION = '###VERSION_PLACEHOLDER###';
 }
