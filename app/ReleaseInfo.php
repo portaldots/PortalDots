7879e90cdb022e4a947bcde0fe9f0585d8ef2d61
SofPyon <sofpyon@hrgrweb.com>
2021-02-23 17:58:16 +0900

    PortalDotsのバージョンを格納しておくためのファイルを作成

---
+++ app/ReleaseInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+/**
+ * PortalDots のリリース情報
+ */
+class ReleaseInfo
+{
+    /**
+     * PortalDots のバージョン
+     *
+     * PortalDots リリース ZIP 作成時、以下の定数にバージョンがセットされます。
+     */
+    public const VERSION = '###VERSION_PLACEHOLDER###';
+}
