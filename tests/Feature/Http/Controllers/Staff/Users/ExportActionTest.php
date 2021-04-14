d3cee2aa15c8354698aab175273eaa72c7665370
hosakou <hosa_kou@hotmail.co.jp>
2021-04-14 22:57:21 +0900

    UsersExportのテスト

---
+++ tests/Feature/Http/Controllers/Staff/Users/ExportActionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Staff\Users;
+
+use App\Eloquents\User;
+use App\Exports\UsersExport;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ExportActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $staff;
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Carbon::setTestNow(new CarbonImmutable('2021-09-14 21:22:23'));
+        CarbonImmutable::setTestNow(new CarbonImmutable('2021-09-14 21:22:23'));
+
+        $this->staff = factory(User::class)->states('staff')->create();
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function ユーザー情報をダウンロードできる()
+    {
+        Excel::fake();
+        $this->actingAs($this->staff)
+            ->withSession(['staff_authorized' => true])
+            ->get(route('staff.users.export'));
+
+        $now = now()->format('Y-m-d_H-i-s');
+
+        Excel::assertDownloaded("users_{$now}.csv", function (UsersExport $export) {
+            return $export->collection()->contains('name', $this->staff->name)
+                && $export->collection()->contains('name', $this->user->name);
+        });
+    }
+}
