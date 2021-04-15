79088d418a481e70779068629ba00cf39350609a
hosakou <hosa_kou@hotmail.co.jp>
2021-04-16 02:01:44 +0900

    スケジュールのテスト書いた

---
+++ tests/Feature/Http/Controllers/Staff/Schedules/ExportActionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Staff\Schedules;
+
+use App\Eloquents\Schedule;
+use App\Eloquents\User;
+use App\Exports\SchedulesExport;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ExportActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @var User
+     */
+    private $staff;
+
+    /**
+     * @var Collection
+     */
+    private $schedules;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->staff = factory(User::class)->state('staff')->create();
+        $this->schedules = factory(Schedule::class, 2)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function 予定をCSVでダウンロードできる()
+    {
+        Excel::fake();
+        $this->actingAs($this->staff)
+            ->withSession(['staff_authorized' => true])
+            ->get(route('staff.schedules.export'));
+
+        $now = Carbon::now()->format('Y-m-d_H-i-s');
+
+        Excel::assertDownloaded("schedules_{$now}.csv", function (SchedulesExport $export) {
+            $names = $this->schedules->pluck('name');
+            return $export->collection()->contains('name', $names[0])
+                && $export->collection()->contains('name', $names[1]);
+        });
+    }
+}
