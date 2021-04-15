8ce1afbe96896835aeaf210fb3bab1128c533c65
hosakou <hosa_kou@hotmail.co.jp>
2021-04-16 00:58:58 +0900

    お知らせのテスト作成

---
+++ tests/Feature/Exports/PagesExportTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature\Exports;
+
+use App\Eloquents\Page;
+use App\Eloquents\Tag;
+use App\Eloquents\User;
+use App\Exports\PagesExport;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class PagesExportTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @var PagesExport
+     */
+    private $pagesExport;
+
+    /**
+     * @var User
+     */
+    private $staff;
+
+    /**
+     * @var Tag
+     */
+    private $tag;
+
+    /**
+     * @var Page
+     */
+    private $page;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pagesExport = App::make(PagesExport::class);
+        $this->staff = factory(User::class)->states('staff')->create([
+            'name' => '野田 一郎',
+        ]);
+        $this->tag = factory(Tag::class)->create([
+            'name' => 'タグです',
+        ]);
+        $this->page = factory(Page::class)->create([
+            'created_by' => $this->staff->id,
+            'updated_by' => $this->staff->id,
+        ]);
+        $this->page->viewableTags()->attach($this->tag->id);
+    }
+
+    /**
+     * @test
+     */
+    public function map_お知らせのフォーマットが正常に行われる()
+    {
+        $this->assertEquals(
+            [
+                $this->page->id,
+                $this->page->title,
+                'タグです',
+                $this->page->body,
+                $this->page->notes,
+                $this->page->created_at,
+                "野田 一郎(ID:{$this->staff->id})",
+                $this->page->updated_at,
+                "野田 一郎(ID:{$this->staff->id})",
+            ],
+            $this->pagesExport->map($this->page)
+        );
+    }
+}
---
+++ tests/Feature/Http/Controllers/Staff/Pages/ExportActionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Staff\Pages;
+
+use App\Eloquents\Page;
+use App\Eloquents\User;
+use App\Exports\PagesExport;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ExportActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @var User
+     */
+    private $staff;
+
+    /**
+     * @var Collection
+     */
+    private $pages;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Carbon::setTestNow(new CarbonImmutable('2021-09-14 21:22:23'));
+        CarbonImmutable::setTestNow(new CarbonImmutable('2021-09-14 21:22:23'));
+
+        $this->staff = factory(User::class)->states('staff')->create();
+
+        $this->pages = factory(Page::class, 2)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function お知らせをCSVでダウンロードできる()
+    {
+        Excel::fake();
+        $this->actingAs($this->staff)
+            ->withSession(['staff_authorized' => true])
+            ->get(route('staff.pages.export'));
+
+        $now = now()->format('Y-m-d_H-i-s');
+
+        Excel::assertDownloaded("pages_{$now}.csv", function (PagesExport $export) {
+            $titles = $this->pages->pluck('title');
+            return $export->collection()->contains('title', $titles[0])
+                && $export->collection()->contains('title', $titles[1]);
+        });
+    }
+}
