7e02b390b2681282e5f02910af231a30e1fcb174
Soji Takahashi / SofPyon <sofpyon@hrgrweb.com>
2021-04-15 23:26:39 +0900

    FormsService内のメソッドのうちフォームエディタ関連のものをFormEditorServiceへ移動

--- app/Services/Forms/FormsService.php
+++ app/Services/Forms/FormsService.php
@@ -6,27 +6,10 @@ namespace App\Services\Forms;
 
 use App\Eloquents\Form;
 use App\Eloquents\User;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class FormsService
 {
-    /**
-     * フォームを更新する
-     *
-     * @param int $form_id フォームID
-     * @param array $form フォーム情報配列
-     */
-    public function updateForm(int $form_id, array $form)
-    {
-        $eloquent = Form::findOrFail($form_id);
-        $form['open_at'] = new Carbon($form['open_at']);
-        $form['close_at'] = new Carbon($form['close_at']);
-        $eloquent->fill($form);
-        $eloquent->save();
-    }
-
     /**
      * フォームを複製する
      *
