1cb2756853f13693aede97829096eda8484fcbd1
Soji Takahashi / SofPyon <sofpyon@hrgrweb.com>
2021-04-25 21:53:52 +0900

    laravel-permissionのPermissionモデルを継承し、全権限リストを作成

--- config/permission.php
+++ config/permission.php
@@ -13,7 +13,7 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => Spatie\Permission\Models\Permission::class,
+        'permission' => App\Eloquents\Permission::class,
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
