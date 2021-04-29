d65abbd93036fb5c834b3529e8df30240d2b280d
Soji Takahashi <sofpyon@hrgrweb.com>
2021-04-29 14:12:11 +0900

    タグの削除機能のテストを権限に対応

--- tests/Feature/Http/Controllers/Staff/Tags/DestroyActionTest.php
+++ tests/Feature/Http/Controllers/Staff/Tags/DestroyActionTest.php
@@ -3,78 +3,116 @@
 namespace Tests\Feature\Http\Controllers\Staff\Tags;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Eloquents\User;
 use App\Eloquents\Circle;
 use App\Eloquents\Tag;
 use App\Eloquents\Page;
 use App\Eloquents\Form;
+use App\Eloquents\Permission;
 
 class DestroyActionTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @var User */
+    private $staff;
+
+    /** @var Circle */
+    private $circle;
+
+    /** @var Tag */
+    private $tag;
+
+    /** @var Page */
+    private $page;
+
+    /** @var Form */
+    private $form;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->staff = factory(User::class)->states('staff')->create();
+
+        $this->circle = factory(Circle::class)->create();
+        $this->tag = factory(Tag::class)->create();
+        $this->page = factory(Page::class)->create();
+        $this->form = factory(Form::class)->create();
+    }
 
     /**
      * @test
      */
     public function tagテーブルから削除すると関連テーブルからも削除される()
     {
-        $staff = factory(User::class)->states('staff')->create();
+        Permission::create(['name' => 'staff.tags.delete']);
+        $this->staff->syncPermissions(['staff.tags.delete']);
 
-        $circle = factory(Circle::class)->create();
-        $tag = factory(Tag::class)->create();
-        $page = factory(Page::class)->create();
-        $form = factory(Form::class)->create();
-
-        $circle->tags()->attach($tag->id);
-        $page->viewableTags()->attach($tag->id);
-        $form->answerableTags()->attach($tag->id);
+        $this->circle->tags()->attach($this->tag->id);
+        $this->page->viewableTags()->attach($this->tag->id);
+        $this->form->answerableTags()->attach($this->tag->id);
 
         $this->assertDatabaseHas('tags', [
-            'id' => $tag->id,
+            'id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseHas('circle_tag', [
-            'circle_id' => $circle->id,
-            'tag_id' => $tag->id,
+            'circle_id' => $this->circle->id,
+            'tag_id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseHas('page_viewable_tags', [
-            'page_id' => $page->id,
-            'tag_id' => $tag->id,
+            'page_id' => $this->page->id,
+            'tag_id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseHas('form_answerable_tags', [
-            'form_id' => $form->id,
-            'tag_id' => $tag->id,
+            'form_id' => $this->form->id,
+            'tag_id' => $this->tag->id,
         ]);
 
-        $response = $this->actingAs($staff)
+        $this->actingAs($this->staff)
             ->withSession(['staff_authorized' => true])
             ->delete(
                 route('staff.tags.destroy', [
-                    'tag' => $tag->id,
+                    'tag' => $this->tag->id,
                 ])
             );
 
         $this->assertDatabaseMissing('tags', [
-            'id' => $tag->id,
+            'id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseMissing('circle_tag', [
-            'circle_id' => $circle->id,
-            'tag_id' => $tag->id,
+            'circle_id' => $this->circle->id,
+            'tag_id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseMissing('page_viewable_tags', [
-            'page_id' => $page->id,
-            'tag_id' => $tag->id,
+            'page_id' => $this->page->id,
+            'tag_id' => $this->tag->id,
         ]);
 
         $this->assertDatabaseMissing('form_answerable_tags', [
-            'form_id' => $form->id,
-            'tag_id' => $tag->id,
+            'form_id' => $this->form->id,
+            'tag_id' => $this->tag->id,
         ]);
+    }
+
+    /**
+     * @test
+     */
+    public function 権限がない場合はタグを削除できない()
+    {
+        $this->actingAs($this->staff)
+            ->withSession(['staff_authorized' => true])
+            ->delete(
+                route('staff.tags.destroy', [
+                    'tag' => $this->tag->id,
+                ])
+            )
+            ->assertForbidden();
     }
 }
