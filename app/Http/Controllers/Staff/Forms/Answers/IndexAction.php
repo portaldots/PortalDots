6925c45c5433d481bd2e9bcfe7b62a777d45344c
Soji Takahashi <sofpyon@hrgrweb.com>
2021-04-29 21:39:15 +0900

    企画参加登録の回答一覧ページにアクセスされた場合、企画情報管理ページへリダイレクトするように

--- app/Http/Controllers/Staff/Forms/Answers/IndexAction.php
+++ app/Http/Controllers/Staff/Forms/Answers/IndexAction.php
@@ -9,6 +9,11 @@ class IndexAction extends Controller
 {
     public function __invoke(Form $form)
     {
+        if (isset($form->customForm) && $form->customForm->type === 'circle') {
+            // このフォームが企画参加登録フォームの場合、企画情報管理ページへリダイレクト
+            return redirect()->route('staff.circles.index');
+        }
+
         return view('staff.forms.answers.index')
             ->with('form', $form);
     }
