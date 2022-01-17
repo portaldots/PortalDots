b68306ecb3b443e81278ae29f1d5e19502022f1e
Soji <6687653+SofPyon@users.noreply.github.com>
2022-01-17 22:31:06 +0900

    サイドウィンドウエディターが読み込み中の場合、スピナーを表示

--- resources/js/v2/components/StaffGridEditor.vue
+++ resources/js/v2/components/StaffGridEditor.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="staff_grid_editor">
+    <div class="staff_grid_editor-loading" v-if="!isLoaded">
+      <i class="fas fa-spinner fa-pulse fa-2x text-primary"></i>
+    </div>
     <iframe
       :src="editorUrl"
       class="staff_grid_editor-frame"
       allowtransparency="true"
-    />
+      @load="handleLoad"
+    ></iframe>
   </div>
 </template>
 
@@ -15,6 +19,16 @@ export default {
       type: String,
       required: true
     }
+  },
+  data() {
+    return {
+      isLoaded: false
+    }
+  },
+  methods: {
+    handleLoad() {
+      this.isLoaded = true
+    }
   }
 }
 </script>
@@ -23,6 +37,13 @@ export default {
 .staff_grid_editor {
   inset: 0;
   position: absolute;
+  &-loading {
+    align-items: center;
+    display: flex;
+    inset: 0;
+    justify-content: center;
+    position: absolute;
+  }
   &-frame {
     border: 0;
     height: 100%;
