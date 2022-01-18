6b8143d94dae8424aa7c82db854693488f49f239
Soji <6687653+SofPyon@users.noreply.github.com>
2022-01-18 23:25:37 +0900

    配布資料管理画面において、サイドウィンドウでの編集機能を設置

--- resources/views/staff/documents/index.blade.php
+++ resources/views/staff/documents/index.blade.php
@@ -33,14 +33,15 @@
                 CSVで出力
             </a>
         </template>
-        <template v-slot:activities="{ row }">
+        <template v-slot:activities="{ row, openEditorByUrl }">
             <form-with-confirm
                 v-bind:action="`{{ route('staff.documents.destroy', ['document' => '%%DOCUMENT%%']) }}`.replace('%%DOCUMENT%%', row['id'])"
                 method="post" v-bind:confirm-message="`配布資料「${row['name']}」を削除しますか？`">
                 @method('delete')
                 @csrf
                 <icon-button
-                    v-bind:href="`{{ route('staff.documents.edit', ['document' => '%%DOCUMENT%%']) }}`.replace('%%DOCUMENT%%', row['id'])"
+                    button
+                    v-on:click="() => openEditorByUrl(`{{ route('staff.documents.edit', ['document' => '%%DOCUMENT%%']) }}`.replace('%%DOCUMENT%%', row['id']))"
                     title="編集">
                     <i class="fas fa-pencil-alt fa-fw"></i>
                 </icon-button>
