1edef6be39fef4b5ed470672c1a8948609319e44
Soji <6687653+SofPyon@users.noreply.github.com>
2022-03-20 16:11:02 +0900

    ダークテーマ時のprimary-lightの色をもう少し濃いめに

--- resources/views/includes/head_ui_theme_dark.blade.php
+++ resources/views/includes/head_ui_theme_dark.blade.php
@@ -1,7 +1,7 @@
 /* Primary */
 color-scheme: dark;
 --color-primary: {{ $uiThemeService->getCssPrimaryColor(1, true) }};
---color-primary-light: {{ $uiThemeService->getCssPrimaryColor(0.2, true) }};
+--color-primary-light: {{ $uiThemeService->getCssPrimaryColor(0.225, true) }};
 --color-focus-primary: {{ $uiThemeService->getCssPrimaryColor(0.4, true) }};
 --color-primary-hover: {{ $uiThemeService->getCssPrimaryColor(0.8, true) }};
 --color-primary-inverse-hover: {{ $uiThemeService->getCssPrimaryColor(0.15, true) }};
