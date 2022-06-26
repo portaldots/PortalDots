9808f2b5683199a3f7d7c72bf25ccdf42879cfba
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2022-06-26 07:13:54 +0000

    インストーラーに問い合わせ先を表示

--- resources/views/install/index.blade.php
+++ resources/views/install/index.blade.php
@@ -11,13 +11,10 @@
 
     <app-container medium>
         <list-view>
-            <list-view-card class="text-center">
-                <p class="text-muted">
-                    <i class="fas fa-door-open fa-2x"></i>
-                </p>
-                <p>
-                    <strong>PortalDots へようこそ！</strong>
-                </p>
+            <list-view-card class="text-center markdown">
+                <h2>
+                    PortalDots へようこそ！
+                </h2>
                 <p>
                     はじめまして！<br>
                     これから 4 ステップに分けて、PortalDots の設定をしましょう。
@@ -40,11 +37,23 @@
                         の設定
                     </steps-list-item>
                 </steps-list>
-                <div class="pt-spacing">
+                <div>
                     <a href="{{ route('install.portal.edit') }}" class="btn is-primary is-wide">
                         <strong>設定をはじめる</strong>
                     </a>
                 </div>
+            </list-view-card>
+        </list-view>
+        <list-view>
+            <template v-slot:title>インストールのサポート</template>
+            <template v-slot:description>PortalDotsのインストールでわからないことがあれば、下記のお問い合わせ先までお気軽にお問い合わせください。</template>
+            <list-view-card class="markdown">
+                <ul>
+                    <li>LINE 公式アカウント: <a href="https://lin.ee/aeee9s9" target="_blank">@aeee9s9</a></li>
+                    <li>メールアドレス: <a href="mailto:support@portaldots.com" target="_blank">support@portaldots.com</a></li>
+                    <li>Twitter: <a href="https://twitter.com/PortalDots" target="_blank">@PortalDots</a></li>
+                </ul>
+                <p>※PortalDots開発チームはボランティアによる活動です。こちらの連絡手段でのサポートは解決を保証するものではありません。予めご了承ください。</p>
             </list-view-card>
         </list-view>
     </app-container>
