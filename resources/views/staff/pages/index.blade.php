f3201b99bcbee7c991bb089f208bb84615011f8e
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-04-05 22:03:22 +0900

    不要なインデントを除去

--- resources/views/staff/pages/index.blade.php
+++ resources/views/staff/pages/index.blade.php
@@ -6,17 +6,17 @@
 
 @section('content')
     <staff-grid api-url="{{ route('staff.pages.api') }}" v-bind:key-translations="{
-                                                    id: 'お知らせID',
-                                                    title: 'タイトル',
-                                                    viewableTags: '閲覧可能なタグ',
-                                                    documents: '関連する配布資料',
-                                                    body: '本文',
-                                                    is_pinned: '固定',
-                                                    is_public: '公開',
-                                                    created_at: '作成日時',
-                                                    updated_at: '更新日時',
-                                                    notes: 'スタッフ用メモ',
-                                                }">
+                            id: 'お知らせID',
+                            title: 'タイトル',
+                            viewableTags: '閲覧可能なタグ',
+                            documents: '関連する配布資料',
+                            body: '本文',
+                            is_pinned: '固定',
+                            is_public: '公開',
+                            created_at: '作成日時',
+                            updated_at: '更新日時',
+                            notes: 'スタッフ用メモ',
+                        }">
         <template v-slot:toolbar>
             <a class="btn is-primary" href="{{ route('staff.pages.create') }}">
                 <i class="fas fa-plus fa-fw"></i>
