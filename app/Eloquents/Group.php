61432ae681916743c36cd4430f06b4f8006bc021
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-04-29 15:26:54 +0900

    団体関連モデル・テーブルを作成

---
+++ app/Eloquents/Group.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Eloquents;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_04_29_150604_create_groups_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_yomi');
+            $table->boolean('is_individual');
+            $table->timestamps();
+        });
+
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->foreignId('group_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->primary(['group_id', 'user_id']);
+        });
+
+        Schema::create('circle_group', function (Blueprint $table) {
+            $table->foreignId('circle_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('group_id')->constrained()->cascadeOnDelete();
+            $table->primary(['circle_id', 'group_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+        Schema::dropIfExists('group_user');
+        Schema::dropIfExists('circle_group');
+    }
+};
