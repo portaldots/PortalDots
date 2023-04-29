06b6ae5ae54e11457206f8c0551c8a8024133541
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-04-29 18:56:22 +0900

    Vue 3ではルートdivタグが不要になったので除去

--- resources/js/v2/components/ListViewStudentIdAndUnivemailInput.vue
+++ resources/js/v2/components/ListViewStudentIdAndUnivemailInput.vue
@@ -1,109 +1,101 @@
 <template>
-  <div>
-    <ListViewFormGroup :labelFor="studentIdInputName">
-      <template #label>{{ studentIdLabel }}</template>
-      <input
-        :id="studentIdInputName"
-        type="text"
-        :class="[
-          'form-control',
-          $slots['invalid-student-id'] ? 'is-invalid' : '',
-        ]"
-        :name="studentIdInputName"
-        required
-        autocomplete="username"
-        v-model="studentId"
-      />
-      <select
-        :class="[
-          'form-control',
-          $slots['invalid-student-id'] ? 'is-invalid' : '',
-        ]"
-        :name="univemailDomainPartInputName"
-        v-model="univemailDomainPart"
-        v-if="appendSelectableDomainPartToStudentIdArea"
+  <ListViewFormGroup :labelFor="studentIdInputName">
+    <template #label>{{ studentIdLabel }}</template>
+    <input
+      :id="studentIdInputName"
+      type="text"
+      :class="[
+        'form-control',
+        $slots['invalid-student-id'] ? 'is-invalid' : '',
+      ]"
+      :name="studentIdInputName"
+      required
+      autocomplete="username"
+      v-model="studentId"
+    />
+    <select
+      :class="[
+        'form-control',
+        $slots['invalid-student-id'] ? 'is-invalid' : '',
+      ]"
+      :name="univemailDomainPartInputName"
+      v-model="univemailDomainPart"
+      v-if="appendSelectableDomainPartToStudentIdArea"
+    >
+      <option
+        v-for="domainPart in allowedDomainParts"
+        :value="domainPart"
+        :key="domainPart"
       >
-        <option
-          v-for="domainPart in allowedDomainParts"
-          :value="domainPart"
-          :key="domainPart"
-        >
-          @{{ domainPart }}
-        </option>
-      </select>
-      <template v-slot:append v-if="appendFixedDomainPartToStudentIdArea">
-        @{{ allowedDomainParts[0] }}
-      </template>
-      <input
-        type="hidden"
-        v-if="appendFixedDomainPartToStudentIdArea"
-        :name="univemailDomainPartInputName"
-        :value="allowedDomainParts[0]"
-      />
-      <template
-        v-slot:invalid
-        v-if="
-          $slots['invalid-student-id'] ||
-          (!allowArbitraryLocalPart && $slots['invalid-univemail'])
-        "
-      >
-        <slot name="invalid-student-id" />
-        <template v-if="!allowArbitraryLocalPart"
-          ><slot name="invalid-univemail"
-        /></template>
-      </template>
-    </ListViewFormGroup>
-    <ListViewFormGroup v-if="allowArbitraryLocalPart">
-      <template #label>{{ univemailLabel }}</template>
-      <input
-        :id="univemailLocalPartInputName"
-        type="text"
-        :class="[
-          'form-control',
-          $slots['invalid-univemail'] ? 'is-invalid' : '',
-        ]"
-        :name="univemailLocalPartInputName"
-        v-model="univemailLocalPart"
-        required
-        autocomplete="username"
-      />
-      <select
-        :class="[
-          'form-control',
-          $slots['invalid-univemail'] ? 'is-invalid' : '',
-        ]"
-        :name="univemailDomainPartInputName"
-        v-model="univemailDomainPart"
-        v-if="allowedDomainParts.length > 1"
-      >
-        <option
-          v-for="domainPart in allowedDomainParts"
-          :value="domainPart"
-          :key="domainPart"
-        >
-          @{{ domainPart }}
-        </option>
-      </select>
-      <template v-slot:append v-if="allowedDomainParts.length === 1">
-        @{{ allowedDomainParts[0] }}
-      </template>
-      <input
-        type="hidden"
-        v-if="allowedDomainParts.length === 1"
-        :name="univemailDomainPartInputName"
-        :value="allowedDomainParts[0]"
-      />
-      <template v-slot:invalid v-if="$slots['invalid-univemail']">
-        <slot name="invalid-univemail" />
-      </template>
-    </ListViewFormGroup>
+        @{{ domainPart }}
+      </option>
+    </select>
+    <template v-slot:append v-if="appendFixedDomainPartToStudentIdArea">
+      @{{ allowedDomainParts[0] }}
+    </template>
     <input
       type="hidden"
-      :name="univemailLocalPartInputName"
-      :value="studentId"
-      v-else
+      v-if="appendFixedDomainPartToStudentIdArea"
+      :name="univemailDomainPartInputName"
+      :value="allowedDomainParts[0]"
     />
-  </div>
+    <template
+      v-slot:invalid
+      v-if="
+        $slots['invalid-student-id'] ||
+        (!allowArbitraryLocalPart && $slots['invalid-univemail'])
+      "
+    >
+      <slot name="invalid-student-id" />
+      <template v-if="!allowArbitraryLocalPart"
+        ><slot name="invalid-univemail"
+      /></template>
+    </template>
+  </ListViewFormGroup>
+  <ListViewFormGroup v-if="allowArbitraryLocalPart">
+    <template #label>{{ univemailLabel }}</template>
+    <input
+      :id="univemailLocalPartInputName"
+      type="text"
+      :class="['form-control', $slots['invalid-univemail'] ? 'is-invalid' : '']"
+      :name="univemailLocalPartInputName"
+      v-model="univemailLocalPart"
+      required
+      autocomplete="username"
+    />
+    <select
+      :class="['form-control', $slots['invalid-univemail'] ? 'is-invalid' : '']"
+      :name="univemailDomainPartInputName"
+      v-model="univemailDomainPart"
+      v-if="allowedDomainParts.length > 1"
+    >
+      <option
+        v-for="domainPart in allowedDomainParts"
+        :value="domainPart"
+        :key="domainPart"
+      >
+        @{{ domainPart }}
+      </option>
+    </select>
+    <template v-slot:append v-if="allowedDomainParts.length === 1">
+      @{{ allowedDomainParts[0] }}
+    </template>
+    <input
+      type="hidden"
+      v-if="allowedDomainParts.length === 1"
+      :name="univemailDomainPartInputName"
+      :value="allowedDomainParts[0]"
+    />
+    <template v-slot:invalid v-if="$slots['invalid-univemail']">
+      <slot name="invalid-univemail" />
+    </template>
+  </ListViewFormGroup>
+  <input
+    type="hidden"
+    :name="univemailLocalPartInputName"
+    :value="studentId"
+    v-else
+  />
 </template>
 
 <script>
