b35cdb225707d7df8b802b73846d0bb3de6448ed
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-02 22:18:01 +0900

    ParticipationTypeをアクティビティログの対象に

--- app/Eloquents/ParticipationType.php
+++ app/Eloquents/ParticipationType.php
@@ -4,10 +4,28 @@ namespace App\Eloquents;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class ParticipationType extends Model
 {
     use HasFactory;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('participation_type')
+            ->logOnly([
+                'id',
+                'name',
+                'description',
+                'users_count_min',
+                'users_count_max',
+                'form_id',
+            ])
+            ->logOnlyDirty();
+    }
 
     protected $fillable = [
         'name',
