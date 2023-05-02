54e367b526dcafe29d1b09d97afc780f4b22f3cb
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-02 22:23:28 +0900

    ListViewEmptyのデザインを見直し、メリハリがあるデザインに

--- resources/js/v2/components/ListViewEmpty.vue
+++ resources/js/v2/components/ListViewEmpty.vue
@@ -26,15 +26,17 @@ export default {
 
 <style lang="scss" scoped>
 .listview-empty {
-  color: $color-muted;
   padding: $spacing-lg 0;
   text-align: center;
   width: 100%;
   &__icon {
     font-size: 2.5rem;
     margin: 0 0 $spacing-md;
+    color: $color-primary;
+    opacity: 0.5;
   }
   &__text {
+    font-size: 1.25rem;
     font-weight: $font-bold;
     margin: 0;
   }
