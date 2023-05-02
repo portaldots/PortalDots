3ae34465f37bb47f38856035472548b42c20306d
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-02 14:12:21 +0900

    ParticipationTypeモデルを作成

---
+++ app/Eloquents/ParticipationType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Eloquents;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ParticipationType extends Model
+{
+    use HasFactory;
+}
