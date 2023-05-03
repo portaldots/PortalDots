bcf7eeb473e32164dbd5f85deb88009cec4d9093
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-03 19:10:06 +0900

    CreateActionTestを修正

--- tests/Feature/Http/Controllers/Circles/BaseTestCase.php
+++ tests/Feature/Http/Controllers/Circles/BaseTestCase.php
@@ -6,26 +6,27 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Carbon\CarbonImmutable;
 use App\Eloquents\Form;
-use App\Eloquents\CustomForm;
+use App\Eloquents\ParticipationType;
 
 abstract class BaseTestCase extends TestCase
 {
     use RefreshDatabase;
 
-    protected $form;
-    protected $customForm;
+    protected ?ParticipationType $participationType;
+    protected ?Form $participationForm;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->form = factory(Form::class)->create([
+        $this->participationForm = factory(Form::class)->create([
             'open_at' => new CarbonImmutable('2020-01-26 11:42:51'),
             'close_at' => new CarbonImmutable('2020-03-26 15:23:31'),
         ]);
-        $this->customForm = factory(CustomForm::class)->create([
-            'type' => 'circle',
-            'form_id' => $this->form->id,
+        $this->participationType = ParticipationType::factory()->create([
+            'form_id' => $this->participationForm->id,
+            'users_count_min' => 1,
+            'users_count_max' => 3,
         ]);
     }
 }
