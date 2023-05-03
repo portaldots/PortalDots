dbccdf4d69c389f9bf2bd2d4025c01b99c9b577d
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-03 14:29:22 +0900

    参加種別を削除した際、同時にフォームも削除

--- app/Http/Controllers/Staff/Circles/ParticipationTypes/DestroyAction.php
+++ app/Http/Controllers/Staff/Circles/ParticipationTypes/DestroyAction.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Staff\Circles\ParticipationTypes;
 
 use App\Eloquents\ParticipationType;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class DestroyAction extends Controller
 {
     public function __invoke(ParticipationType $participationType)
     {
-        $participationType->delete();
-        return redirect(route('staff.circles.index'));
+        return DB::transaction(function () use ($participationType) {
+            $participationType->form->delete();
+            $participationType->delete();
+            return redirect(route('staff.circles.index'));
+        });
     }
 }
