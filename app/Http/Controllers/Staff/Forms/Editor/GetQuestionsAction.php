b86cf95521cfb10901810d21a335b5252e0edf37
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2023-05-04 10:59:04 +0900

    フォームエディターの表示がエラーになる問題を修正

--- app/Http/Controllers/Staff/Forms/Editor/GetQuestionsAction.php
+++ app/Http/Controllers/Staff/Forms/Editor/GetQuestionsAction.php
@@ -28,6 +28,7 @@ class GetQuestionsAction extends Controller
             ];
         })->toArray();
 
+        $permanent_questions = [];
         if (isset($form->participationType)) {
             $permanent_questions = [
                 [
